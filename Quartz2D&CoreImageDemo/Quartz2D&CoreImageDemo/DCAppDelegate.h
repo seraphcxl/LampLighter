9c91cf853a8d327afb58c17b17133f1412e9223d
seraphcxl <seraph.cxl@gmail.com>
2014-06-23 16:24:13 +0800

    Add Quartz2D&CoreImageDemo app and LampLighter fwk

---
+++ Quartz2D&CoreImageDemo/Quartz2D&CoreImageDemo/DCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DCAppDelegate.h
+//  Quartz2D&CoreImageDemo
+//
+//  Created by Derek Chen on 6/23/14.
+//  Copyright (c) 2014 Derek Chen. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface DCAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ Quartz2D&CoreImageDemo/Quartz2D&CoreImageDemo/DCAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  DCAppDelegate.m
+//  Quartz2D&CoreImageDemo
+//
+//  Created by Derek Chen on 6/23/14.
+//  Copyright (c) 2014 Derek Chen. All rights reserved.
+//
+
+#import "DCAppDelegate.h"
+
+@implementation DCAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
